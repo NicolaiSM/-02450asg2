7ad36ade03ef9e3eadfa39007a0951639b4d5bd2
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-11-17 13:04:19 +0100

    Make get data more generic and add options for specifying onehot encoding and drop_columns

--- src/__main__.py
+++ src/__main__.py
@@ -2,9 +2,14 @@ from data_handler import get_data
 #from ann_reg import ANN_trainer
 from baseline import baseline
 
-data, target, N, M, attributeNames, CV = get_data(10)
 
-print(baseline(data, target, N, M, attributeNames, CV))
+drop_columns = ["name", "mainhue", "topleft", "botright", "landmass", "zone", "language", "religion", "colours", "red", "green", "blue", "gold", "white", "black", "orange"]
+
+onehot_classes = ["landmass", "zone", "language", "religion"]
+
+data, target, N, M, attributeNames, CV = get_data(10, onehot_classes, drop_columns)
+
+baseline(data, target, N, M, attributeNames, CV)
 
 """
 n_hiddens = [1, 2, 4]
